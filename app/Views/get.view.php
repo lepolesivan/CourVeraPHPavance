15b18c98cafdd6c785322a200fe5511db936d2ad
lepolesivan <ivansomo27@gmail.com>
2024-09-09 14:39:37 +0200

    mise à jour du cours

--- app/Views/get.view.php
+++ app/Views/get.view.php
@@ -1,10 +1,12 @@
 <?php
     include '../Views/partials/head.php';
 ?>
-    <a href="recupget.view.php?prenom=Abdou&age=20">Utilisateur</a>
-    <a href="recupget.view.php?article=jean&couleur=bleu&prix=30">Jean bleu</a>
-    <a href="recupget.view.php?article=robe&couleur=rouge&prix=30">Robe rouge</a>
-    <a href="recupget.view.php?article=pull&couleur=blanc&prix=30">Pull blanc</a>
+    <a href="traitementexo.view.php?prenom=Abdou&age=20">Utilisateur</a>
+    <a href="traitementexo.view.php?article=jean&couleur=bleu&prix=30">Jean bleu</a>
+    <a href="traitementexo.view.php?article=robe&couleur=rouge&prix=30">Robe rouge</a>
+    <a href="traitementexo.view.php?article=pull&couleur=blanc&prix=30">Pull blanc</a>
+    <!-- Exercice 1 -->
+    <a href="traitementexo.view.php?article=Julien&couleur=Defreitas&prix=45">Julien</a>
 <?php
     include '../Views/partials/footer.php';
 ?>
--- app/Views/traitementexo.view.php
+++ app/Views/traitementexo.view.php
@@ -1,4 +1,5 @@
 <?php
+include '../Views/partials/head.php';
 //inclure head et footer
 
 /** Exercice 1 : Affichage des paramètres
@@ -6,24 +7,35 @@
  *  Objectif : Afficher tous les paramètres passés dans l'URL
  * 
  * 
- *  1 . En gardant la page de rendu HTML, utiliser les informations de cette page pour les passer en paramètres d'URL
+ *  1 . En gardant la page get.view.php, 
+ *  utiliser les informations de cette page pour les passer en paramètres d'URL
  * 
- *  2 . Créer un 2nd fichier nommé : traitement.php où $_GET doit être récupéré, vérifié et ses paramètres affichés sur le navigateur 
+ *  2 . Dans le fichier nommé : traitementexo.view.php où $_GET doit être récupéré, vérifié les paramètres affichés sur le navigateur 
  * 
- */
-
-/** Validation des paramètres
+ *  3 . S'assurer que les paramètres article,couleur et prix sont présents dans l'URL
  * 
- *  Objectif : Valider les paramètres reçus dans l'URL et afficher un message d'erreur si un paramètre est manquant
- * 
- *  1 . S'assurer que les paramètres article,couleur et prix sont présents dans l'URL
- * 
- *  2. Si un ou plusieurs paramètres manquent, afficher un message d'erreur spécifique pour chacun
+ *  4. Si un ou plusieurs paramètres manquent, afficher un message d'erreur spécifique pour chacun
  * 
  *  Exemple : "<p> Le paramètre 'Article' est manquant'"
  */
 
-/** Exercice 3 : Conversion du prix
+echo "<h1>Exercice traitement GET</h1>";
+echo "<h2>Exercice 1</h2>";
+
+if (isset($_GET['article']) && isset($_GET['couleur']) && isset($_GET['prix'])) {
+    echo '<p> le paramètre ' . htmlspecialchars($_GET['article']) . ' est présent </p>';
+    echo '<p> le paramètre ' . htmlspecialchars($_GET['couleur']) . ' est présent </p>';
+    echo '<p> le paramètre ' . htmlspecialchars($_GET['prix']) . ' est présent </p>';
+} else if (!isset($_GET['article'])) {
+    echo '<p> Le paramètre ' . htmlspecialchars($_GET['article']) . 'est absent </p>';
+} else if (!isset($_GET['couleur'])) {
+    echo '<p> Le paramètre ' . htmlspecialchars($_GET['couleur']) . 'est absent </p>';
+} else if (!isset($_GET['prix'])) {
+    echo '<p> Le paramètre ' . htmlspecialchars($_GET['prix']) . 'est absent </p>';
+}
+
+
+/** Exercice 2 : Conversion du prix
  * 
  *  Objectif : Convertir le prix reçu dans l'URL de dollars à euros (utiliser le taux de conversion de 1 dollars = 0.92 euros)
  * 
@@ -34,7 +46,22 @@
  *  3 . Afficher le prix converti
  */
 
-/** Exercice 4 : Affichage d'un message personnalisé
+echo "<h2>Exercice 2</h2>";
+
+if(isset($_GET['prix'])){
+    $prix = floatval($_GET['prix']);
+
+    $result = dollarsToEuro($prix);
+    echo "<p>Prix en euros : $result</p>";
+}
+
+function dollarsToEuro($prix){
+    $prixEuro = $prix * 0.92;
+    return $prixEuro;
+}
+
+
+/** Exercice 3 : Affichage d'un message personnalisé
  * 
  *  Objectif : Afficher un message personnalisé en fonction de la couleur passée dans l'URL
  * 
@@ -44,7 +71,29 @@
  * 
  */
 
-/** Exercice 5 : Vérification du prix minimum
+ echo "<h2>Exercice 3</h2>";
+
+ if(isset($_GET['couleur'])){
+    $couleur = htmlspecialchars($_GET['couleur']);
+    switch ($couleur) {
+        case 'jaune':
+            echo "<p>Cette couleur me fait penser au soleil !</p>";
+            break;
+        case 'bleu':
+            echo "<p>Cette couleur me rappelle l'océan.</p>";
+            break;
+        case 'rouge':
+            echo "<p>Cette couleur évoque la passion !</p>";
+            break;
+        case 'blanc':
+            echo "<p>Cette couleur représente la pureté.</p>";
+            break;
+        default:
+            echo "<p>Couleur inconnue.</p>";
+    }
+ }
+
+/** Exercice 4 : Vérification du prix minimum
  * 
  *  Objectif : Vérifier si le prix reçu dans l'URL est supérieur à un montant minimum et afficher un message appriprié
  * 
@@ -54,3 +103,19 @@
  * 
  *  3 . Afficher un message indiquant si le prix est suffisant ou non 
  */
+
+
+ echo "<h2>Exercice 4</h2>";
+
+ $minPrice = 20;
+
+ if(isset($_GET['prix'])){
+    $price = floatval($_GET['prix']);
+    if($price < $minPrice){
+        echo "<p>mon prix est plus petit que $minPrice </p>";
+    } else {
+        echo "<p>mon prix est plus grand que $minPrice </p>";
+    }
+ }
+
+ include '../Views/partials/footer.php';
